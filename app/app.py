cb5e1d3a9fa8714a9cfdebfefff8b93c99c237ae
Paulo Santiago <p.santiago796@gmail.com>
2026-03-16 16:46:22 -0400

    create basic login page

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return "Hello, Flask!"
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
  
 if __name__ == '__main__':
    app.run(debug=True)
